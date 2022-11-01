3c1530cadc2aabac7d36b9034b78d939128f3b46
Prof-X-Samson <olowoyosamson@gmail.com>
2022-10-31 21:15:28 -0400

    Helper Script

---
+++ docker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Author: Samson Olowoyo
+#Date: Oct 31 2022
+#Description: A Simple script for Docker installation.
+yum update -y
+
+if [ $? -ne 0 ]
+then
+    echo “someting went wrong during the upgrade!”
+    exit 2
+else
+    yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+    if [ $? -ne 0 ]
+    then
+        echo “someting went wrong during the previous version remove!”
+        exit 2
+    else
+        yum install -y yum-utils
+        yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+        if [ $? -ne 0 ]
+        then
+            echo “someting went wrong while setting up the repository, please check the commands!”
+            exit 2
+        else
+            yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+            if [ $? -ne 0 ]
+            then
+                echo “someting went wrong during the installation, , please check the command”
+                exit 2
+            else
+                systemctl start docker
+                docker run hello-world
+                if [ $? -ne 0 ]
+                then
+                    echo “Docker has been succesfully installed but having some problem to start!”
+                    exit 2
+                else
+                    echo “Docker has been succesfully installed and running !”
+                fi
+            fi
+        fi
+    fi
+fi
---
+++ jenkins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Author: Samson Olowoyo
+#Date: Oct 31 2022
+#Description: A script for Jenkins Installation
+
+yum install java-11-openjdk-devel -y
+
+if [ $? -ne 0 ]
+then
+    echo “someting went wrong during the java installation!”
+    exit 2
+else
+    curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+    rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+    if [ $? -ne 0 ]
+    then
+        echo “someting went wrong when adding jenkins repository!”
+        exit 2
+    else
+        yum install jenkins -y
+        if [ $? -ne 0 ]
+        then
+            echo “someting went wrong during the Jenkins installation!”
+            exit 2
+        else
+            systemctl start jenkins
+            sudo systemctl enable jenkins
+            if [ $? -ne 0 ]
+            then
+                echo “someting went wrong when starting Jenkins”
+                exit 2
+            else
+                firewall-cmd --permanent --zone=public --add-port=8080/tcp
+                firewall-cmd --reload               
+                systemctl status jenkins
+                echo “Jenkins has been succesfully installed and running !”
+                
+            fi
+        fi
+    fi
+fi
